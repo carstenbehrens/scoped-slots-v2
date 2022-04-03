855443082fb8b9090ddc2aa26abfe22b5b5832c0
carstenbehrens <carsten.behrens1@googlemail.com>
2022-04-03 20:02:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <child-component>
+    <template v-slot:default="slotProps">
+      Now we can use the data from the child component here: {{ slotProps.example }}
+    </template>
+  </child-component>
+</template>
+
+<script>
+import ChildComponent from "./components/ChildComponent.vue";
+
+export default {
+  name: 'App',
+  components: {
+    ChildComponent
+  }
+}
+</script>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot v-bind:example="example">
+    </slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ChildComponent',
+  data() {
+    return {
+      example: 'just some data...'
+    }
+  }
+}
+</script>
